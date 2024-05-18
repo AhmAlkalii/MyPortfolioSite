37a37c05bad5de049631c519e7754a46a13bd3ca
Ahmed Alkali Mohammed <hmkalkali@gmail.com>
2024-05-18 11:20:24 +0200

    Done

---
+++ src/helpers/ProjectList.jsx
@@ -0,0 +1,45 @@
+import cvgen from "../assets/CVgen.png";
+import facial from "../assets/Facial.jpg";
+import quiz from "../assets/quiz.png";
+import dcu from "../assets/dcusite.png"
+import todo from "../assets/Todolist.png"
+import rent from "../assets/carent.png"
+import rnate from "../assets/rnate.jpeg"
+export const ProjectList = [
+  {
+    name: "CV Generation Website Frondend + Backend",
+    image: cvgen,
+    skills: "Python,Angular,OpenAIAPI,Flask,SQL",
+  },
+  {
+    name:"Car Rental System (Full Stack)",
+    image: rent,
+    skills: "Angular, Flask, SQLite, Azure"
+  },
+  {
+    name: "Facial Recognition Attendance System",
+    image: facial,
+    skills: "Python",
+  },
+  {
+    name: "Car Rental Mobile Application(FullStack)",
+    image: rnate,
+    skills: "React Native, Node and Express JS and Sqlite3",
+  },
+  {
+    name: "DCU Fan Website",
+    image: dcu,
+    skills: "JavaScript,HTML,CSS",
+  },
+  {
+    name: "QuizSite",
+    image: quiz,
+    skills: "React",
+  },
+  {
+    name: "TodoList",
+    image: todo,
+    skills: "React",
+  },
+
+];
